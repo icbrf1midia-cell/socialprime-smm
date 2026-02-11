2b3a18ea14d6ac1d74e65b30302c5dffb0517386
Bruno Adriano <futuroevoluido@gmail.com>
2026-02-11 18:52:21 -0300

    style: Add bottom padding to the main content area.

--- App.tsx
+++ App.tsx
@@ -63,7 +63,7 @@ const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
       <div className="flex-1 flex flex-col h-full overflow-hidden relative bg-background-light dark:bg-background-dark">
         <Header onToggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
-        <main className="flex-1 overflow-y-auto p-4 md:p-6 scrollbar-hide">
+        <main className="flex-1 overflow-y-auto p-4 md:p-6 pb-32 scrollbar-hide">
           {children}
         </main>
       </div>
